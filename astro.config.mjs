318dbbf6cf7de2c943a3b2490fe2391e2d17860e
lucidmach <lucidmach@gmail.com>
2026-07-31 12:15:52 +1000

    local network hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+server: { host: true },
   vite: {
     plugins: [tailwindcss()]
   }
